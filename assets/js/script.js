b00500ec11636afc3817bfe256c4446da4bdb174
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-06-14 12:41:37 +0100

    added settimeout function to reset notification count

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,25 @@ const Mark = document.getElementById('Mark')
 const Angela = document.getElementById('Angela')
 const Jacob = document.getElementById('Jacob')
 
+// Resets page after 5 seconds
+setTimeout(resetApp, 5000)
+
 function updateNotificationCount () {
     // Sets counter back to zero
-NotificationNumber.textContent = 0
+    NotificationNumber.textContent = 0
 
+    // Resets background color to white once button is clicked
+    Mark.style.cssText = 'background-color: #fafafa;'
+    Angela.style.cssText = 'background-color: #fafafa;'
+    Jacob.style.cssText = 'background-color: #fafafa;' 
+}
 
-// Resets background color to white once button is clicked
-Mark.style.cssText = 'background-color: #fafafa;'
-Angela.style.cssText = 'background-color: #fafafa;'
-Jacob.style.cssText = 'background-color: #fafafa;' 
+function resetApp () {
+Mark.style.cssText = 'background-color: #E5E4E2;'
+Angela.style.cssText = 'background-color: #E5E4E2;'
+Jacob.style.cssText = 'background-color: #E5E4E2;' 
+
+// Sets counter back to three
+NotificationNumber.textContent = 3
 }
 
